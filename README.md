64abd9399398445b0f9ca6a0f0772a643595b6e2
Guy <guy.raz@student.winstonprep.edu>
2023-03-25 12:53:02 +0000

    Initialised repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hang Man Game
+
+## Dependencies
+
+There are no dependencies except for `random`, which is included in the standard library.
+
+## Tutorial
+
+To run, simply run the script with python. **Note: Python 3.0 and higher is required**
---
+++ main.py
@@ -0,0 +1,41 @@
+import random
+
+def choose_word():
+    words = ["python", "javascript", "ruby", "java", "swift", "php", "html", "css", "csharp", "go"]
+    return random.choice(words)
+
+def play_game():
+    word = choose_word()
+    word_letters = set(word)
+    alphabet = set('abcdefghijklmnopqrstuvwxyz')
+    used_letters = set()
+
+    lives = 6
+
+    while len(word_letters) > 0 and lives > 0:
+        print("You have", lives, "lives left and you have used these letters: ", ' '.join(used_letters))
+
+        word_list = [letter if letter in used_letters else '_' for letter in word]
+        print("Current word: ", ' '.join(word_list))
+
+        user_letter = input("Guess a letter: ").lower()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                lives = lives - 1
+                print("Letter is not in word.")
+        
+        elif user_letter in used_letters:
+            print("You have already used that letter. Guess another one.")
+        
+        else:
+            print("Invalid character. Please try again.")
+    
+    if lives == 0:
+        print("Sorry, you died. The word was", word)
+    else:
+        print("You guessed the word", word, "!!")
+
+play_game()
